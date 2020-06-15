2e7489afd5c43c238f5bf37933f58efe4071b6dd
elliotjermy <elliot.jermy@outlook.com>
2020-06-15 20:35:05 +0100

    added brewing pt1

---
+++ Assets/Elliots Crap/InventoryManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+    public InventorySlot brimstoneSlot;
+    public InventorySlot crystalSlot;
+    public InventorySlot nitrogenSlot;
+    public InventorySlot mushroomSlot;
+    [HideInInspector] public string selectedIngredient;
+
+    public int brimstoneHeld;
+    public int crystalHeld;
+    public int nitrogenHeld;
+    public int mushroomHeld;
+    public int gunpowderHeld;
+
+    public Text brimstoneAmount;
+    public Text crystalAmount;
+    public Text nitrogenAmount;
+    public Text mushroomAmount;
+    public Text gunpowderAmount;
+
+    public void SlotSelected(string ingredient)
+    {
+        if (ingredient == "brimstone")
+        {
+            crystalSlot.Unhighlight();
+            nitrogenSlot.Unhighlight();
+            mushroomSlot.Unhighlight();
+            selectedIngredient = brimstoneSlot.ingredient;
+        }
+        if (ingredient == "crystal")
+        {
+            brimstoneSlot.Unhighlight();
+            nitrogenSlot.Unhighlight();
+            mushroomSlot.Unhighlight();
+            selectedIngredient = crystalSlot.ingredient;
+        }
+        if (ingredient == "nitrogen")
+        {
+            brimstoneSlot.Unhighlight();
+            crystalSlot.Unhighlight();
+            mushroomSlot.Unhighlight();
+            selectedIngredient = nitrogenSlot.ingredient;
+        }
+        if (ingredient == "mushroom")
+        {
+            brimstoneSlot.Unhighlight();
+            crystalSlot.Unhighlight();
+            nitrogenSlot.Unhighlight();
+            selectedIngredient = mushroomSlot.ingredient;
+        }
+    }
+
+    void Update()
+    {
+        brimstoneAmount.text = "" + brimstoneHeld;
+        crystalAmount.text = "" + crystalHeld;
+        nitrogenAmount.text = "" + nitrogenHeld;
+        gunpowderAmount.text = "" + gunpowderHeld;
+        mushroomAmount.text = "" + mushroomHeld;
+    }
+
+    public void BrewPotion()
+    {
+        if(gunpowderHeld >= 1)
+        {
+            if (selectedIngredient == "brimstone" && brimstoneHeld >= 1)
+            {
+                brimstoneHeld--;
+                gunpowderHeld--;
+            }
+            if (selectedIngredient == "crystal" && crystalHeld >= 1)
+            {
+                crystalHeld--;
+                gunpowderHeld--;
+            }
+            if (selectedIngredient == "nitrogen" && nitrogenHeld >= 1)
+            {
+                nitrogenHeld--;
+                gunpowderHeld--;
+            }
+            if (selectedIngredient == "mushroom" && mushroomHeld >= 1)
+            {
+                mushroomHeld--;
+                gunpowderHeld--;
+            }
+        }   
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public GameObject black;
+    private Animator screenFade;
+    void Start()
+    {
+        screenFade = black.gameObject.GetComponent<Animator>();
+    }
+    public void LoadScene(string scene)
+    {
+        StartCoroutine(FadeOut(scene));
+    }
+    IEnumerator FadeOut(string scene)
+    {
+        screenFade.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(0.8f);
+        SceneManager.LoadScene(scene);
+    }
+}
